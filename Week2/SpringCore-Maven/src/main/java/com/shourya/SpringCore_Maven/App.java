0080afdba84fd5d254fd9f5c2945c1b3f28ee052
Shourya <shouryagupta.work@gmail.com>
2026-07-06 15:15:30 +0530

    added week2 exercises

---
+++ Week2/SpringCore-Maven/src/main/java/com/shourya/SpringCore_Maven/App.java
@@ -0,0 +1,10 @@
+package com.shourya.SpringCore_Maven;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Week2/SpringJPA-Hibernate/src/main/java/com/shourya/SpringJPA_Hibernate/App.java
@@ -0,0 +1,10 @@
+package com.shourya.SpringJPA_Hibernate;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
