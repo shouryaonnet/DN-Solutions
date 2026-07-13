121865c5114aafb3c7d19036353160899a482e58
Shourya <shouryagupta.work@gmail.com>
2026-07-13 14:23:49 +0530

    for week 4, made loan microservice

---
+++ week4/loan/src/main/java/com/cognizant/loan/LoanController.java
@@ -0,0 +1,26 @@
+package com.cognizant.loan.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/loans")
+public class LoanController {
+
+    @GetMapping("/{number}")
+    public Map<String,Object> getLoan(@PathVariable String number){
+
+        Map<String,Object> loan=new HashMap<>();
+
+        loan.put("number",number);
+        loan.put("type","Car");
+        loan.put("loan",400000);
+        loan.put("emi",3258);
+        loan.put("tenure",18);
+
+        return loan;
+    }
+
+}
