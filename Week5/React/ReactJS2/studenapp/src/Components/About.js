f04ecefdcd4f68cb76c9c37ef5892ed474cce5f6
Shourya <shouryagupta.work@gmail.com>
2026-07-14 10:09:22 +0530

    in week 5, added for reactjs1 and completed the reactjs2 exercise

---
+++ Week5/React/ReactJS2/studenapp/src/Components/About.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class About extends Component {
+  render() {
+    return (
+      <h1>Welcome to the About page of Student Management Portal</h1>
+    );
+  }
+}
+
+export default About;
---
+++ Week5/React/ReactJS2/studenapp/src/Components/Contact.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+  render() {
+    return (
+      <h1>Welcome to the Contact page of Student Management Portal</h1>
+    );
+  }
+}
+
+export default Contact;
