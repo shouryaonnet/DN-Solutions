ca58b830cb8fc043a0cac3ebe54f77f81d5d4660
Shourya <shouryagupta.work@gmail.com>
2026-07-15 09:59:44 +0530

    in week 5, completed reactjs4 exercise

---
+++ Week5/React/ReactJS4/blogapp/src/Posts.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Post from "./Post";
+
+class Posts extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            posts: []
+        };
+    }
+
+    loadPosts() {
+        fetch("https://jsonplaceholder.typicode.com/posts")
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    posts: data
+                });
+            });
+    }
+
+    componentDidMount() {
+        this.loadPosts();
+    }
+
+    componentDidCatch(error, info) {
+        alert(error);
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.posts.map(post => (
+                    <div key={post.id}>
+                        <h2>{post.title}</h2>
+                        <p>{post.body}</p>
+                        <hr />
+                    </div>
+                ))}
+            </div>
+        );
+    }
+}
+
+export default Posts;
