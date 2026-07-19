6d2bbb92eebc6d6a6d5b76ee2a43709ac60f53be
Shourya <shouryagupta.work@gmail.com>
2026-07-19 08:28:46 +0530

    added reactjs4

--- Week5/React/ReactJS4/blogapp/src/Posts.js
+++ Week5/React/ReactJS4/blogapp/src/Posts.js
@@ -2,10 +2,8 @@ import React from "react";
 import Post from "./Post";
 
 class Posts extends React.Component {
-
     constructor(props) {
         super(props);
-
         this.state = {
             posts: []
         };
